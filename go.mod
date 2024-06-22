caedeec42ddcf9ae5e18aa0084dde0e2b85750b4
samo <jovanesammy@gmail.com>
2024-06-22 02:16:01 -0400

    added write out method and created another invertedindex for tags

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module samodon/search
 
 go 1.22.4
 
-require github.com/kljensen/snowball v0.9.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/kljensen/snowball v0.9.0 // indirect
+	github.com/yuin/goldmark v1.7.2 // indirect
+	go.abhg.dev/goldmark/frontmatter v0.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- search.go
+++ search.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sort"
 	"strings"
+	"unicode"
 
 	"github.com/kljensen/snowball"
+	"github.com/kljensen/snowball/english"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/text"
+	"go.abhg.dev/goldmark/frontmatter"
 )
 
 type document struct {
 	documentloc string
 	content     string
-}
-
-func test(doc document) {
+	tags        []string
 }
 
 func getdocuments(dirname string) []document {
@@ -25,10 +29,10 @@ func getdocuments(dirname string) []document {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() && strings.HasSuffix(info.Name(), "txt") {
+		if !info.IsDir() && strings.HasSuffix(info.Name(), "md") {
 			var tmpdocument document
 			tmpdocument.documentloc = path
-			tmpdocument.content, err = readfile(tmpdocument.documentloc)
+			tmpdocument.content, tmpdocument.tags, err = readfile(tmpdocument.documentloc)
 			if err != nil {
 				fmt.Print(err)
 				return nil
@@ -46,12 +50,31 @@ func getdocuments(dirname string) []document {
 // TODO
 // modify this to return date, tags, topic, etc
 // look into processing this in chunks for improved efficiency
-func readfile(path string) (string, error) {
+func readfile(path string) (string, []string, error) {
 	content, err := os.ReadFile(path)
 	if err != nil {
-		return "Error occured", err
+		return "Error occured", nil, err
 	}
-	return string(content), err
+
+	md := goldmark.New(
+		goldmark.WithExtensions(
+			&frontmatter.Extender{
+				Mode: frontmatter.SetMetadata,
+			},
+		),
+	)
+
+	root := md.Parser().Parse(text.NewReader(content))
+	doc := root.OwnerDocument()
+	meta := doc.Meta()
+	var tags []string
+	if meta["tags"] != nil {
+		strtags := fmt.Sprint(meta["tags"])
+		strtags = strings.Trim(strtags, "[]")
+		tags = strings.Fields(strtags)
+	}
+
+	return stripPunctuation(string(content)), tags, err
 }
 
 func contains(slice []string, item string) bool {
@@ -66,10 +89,18 @@ func contains(slice []string, item string) bool {
 // TODO
 // Sort the words in alphabetical order
 // only return the root words
-func tokenize(documents []document) []string {
+func tokenize(documents []document) ([]string, []string) {
 	var checkedwords []string
 	var uniquewords []string
+	var uniquetags []string
 	for _, document := range documents {
+		if document.tags != nil {
+			for _, tag := range document.tags {
+				if !contains(uniquetags, tag) {
+					uniquetags = append(uniquetags, tag)
+				}
+			}
+		}
 		words := strings.Fields(document.content)
 		for _, word := range words {
 			if !contains(checkedwords, word) {
@@ -79,41 +110,101 @@ func tokenize(documents []document) []string {
 		}
 	}
 	sort.Strings(uniquewords)
-	return uniquewords
+	return uniquewords, uniquetags
 }
 
-func createindex(words []string, documents []document) map[string][]string {
-	invertedindex := make(map[string][]string)
+func createindex(words []string, tags []string, documents []document) (map[string][]string, map[string][]string) {
+	wordindex := make(map[string][]string)
+	fmt.Printf("Indexing...")
 	for _, word := range words {
 		for _, document := range documents {
 			if contains(strings.Fields(document.content), word) {
 				stemmed, _ := snowball.Stem(word, "english", true)
-				invertedindex[stemmed] = append(invertedindex[stemmed], document.documentloc)
+				wordindex[stemmed] = append(wordindex[stemmed], document.documentloc)
 			}
 		}
 	}
-	return invertedindex
+	tagsindex := make(map[string][]string)
+	for _, tag := range tags {
+		for _, document := range documents {
+			if contains(document.tags, tag) {
+				// fmt.Println(tag)
+				// fmt.Println(document.documentloc)
+				tagsindex[tag] = append(tagsindex[tag], document.documentloc)
+			}
+		}
+	}
+	return wordindex, tagsindex
 }
 
-func getresults(words []string) []document {
-	words = append(words, "test")
-	return nil
+func stripPunctuation(text string) string {
+	var sb strings.Builder
+	for _, c := range text {
+		if !unicode.IsPunct(c) && !unicode.IsDigit(c) && !unicode.IsNumber(c) && !unicode.IsSymbol(c) {
+			sb.WriteRune(c)
+		}
+	}
+	return sb.String()
 }
 
-func main() {
-	directory := "."
+func getresults(invertedindex map[string][]string) [][]string {
 	reader := bufio.NewReader(os.Stdin)
-
-	documents := getdocuments(directory)
-
-	uniquewords := (tokenize(documents))
-	invertedindex := createindex(uniquewords, documents)
-	// fmt.Println(invertedindex)
 
 	fmt.Print("Enter search term:")
 	input, _ := reader.ReadString('\n')
-	input, _ = snowball.Stem(input, "english", true)
-	input = strings.TrimSpace(input)
-	print(input)
-	fmt.Println(invertedindex[input])
+
+	var results [][]string
+	wordlist := strings.Fields(input)
+
+	for _, word := range wordlist {
+		word = english.Stem(word, true)
+
+		if invertedindex[word] != nil {
+			path := invertedindex[word]
+			results = append(results, path)
+		}
+
+	}
+	return results
+}
+
+func writeout(invertedindex map[string][]string, tagindex map[string][]string) {
+	file, err := os.Create("wordindex")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+	err = encoder.Encode(invertedindex)
+	if err != nil {
+		panic(err)
+	}
+	file, err = os.Create("tagindex")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	encoder = json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+	err = encoder.Encode(tagindex)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	directory := "/Users/samo/Library/Mobile Documents/com~apple~CloudDocs/Documents/Obsidian Vaults/Projects/Notes/Atomic/"
+
+	documents := getdocuments(directory)
+
+	uniquewords, uniquetags := (tokenize(documents))
+
+	// fmt.Println(uniquetags)
+	invertedindex, tagindex := createindex(uniquewords, uniquetags, documents)
+	// fmt.Print(tagindex)
+	writeout(invertedindex, tagindex)
+	// tags inverted index
 }
